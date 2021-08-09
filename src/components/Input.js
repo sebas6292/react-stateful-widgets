b93a3fd5dc0de6576aee614525d8f87c343de376
Sebastian Matthew Leal <sebastianmatthewleal@Sebastians-MacBook-Pro.local>
2021-08-09 15:12:38 -0700

    input completed

--- src/components/Input.js
+++ src/components/Input.js
@@ -44,11 +44,13 @@ export default function Input() {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
-
+      // console.log(value)
     /* STEP 4 */
+      setInputValue(value)
   };
   const reset = () => {
     /* STEP 5 */
+    setInputValue('')
   };
 
   const style = {
@@ -62,7 +64,7 @@ export default function Input() {
       <h2>Input</h2>
       <div id='output' style={style}>{inputValue.toUpperCase()}</div> {/* STEP 3 */}
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
+        <input id='input' type='text' onChange={changeInput} value={inputValue}/> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
